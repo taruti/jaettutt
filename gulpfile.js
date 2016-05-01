4f5c3bda8292defaece5b6b061b9fa3e6df6ee61
Taru Karttunen <taruti@taruti.net>
2016-05-01 12:57:39 +0300

    gulpfile.js: Concat and copy javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('lint', function() {
     return gulp.src('js/*.js')
@@ -8,8 +10,16 @@ gulp.task('lint', function() {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint']);
+gulp.task('scripts-dev', function() {
+  return gulp.src('js/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('debug_dist'));
 });
 
-gulp.task('default', ['lint', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts-dev']);
+});
+
+gulp.task('default', ['lint', 'scripts-dev', 'watch']);
