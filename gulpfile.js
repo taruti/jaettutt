63e2bf30ba0079aa3c0a13f407c384253b4d864c
Taru Karttunen <taruti@taruti.net>
2016-05-02 10:19:20 +0300

    gulpfile.js: format

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,23 @@ var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task('lint', function () {
+  return gulp.src('js/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('scripts-dev', function() {
-  return gulp.src('js/*.js')
+gulp.task('scripts-dev', function () {
+  return gulp.src('js/*.js7')
     .pipe(sourcemaps.init())
-      .pipe(concat('all.js'))
+    .pipe(concat('all.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('debug_dist'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts-dev']);
+gulp.task('watch', function () {
+  gulp.watch('js/*.js', ['lint', 'scripts-dev']);
 });
 
 gulp.task('default', ['lint', 'scripts-dev', 'watch']);
