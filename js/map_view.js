9856e208aec1be08f7243b4f91bfeb8425e65332
Taru Karttunen <taruti@taruti.net>
2016-06-07 17:44:34 +0300

    Also draw hex grids

--- js/map_view.js
+++ js/map_view.js
@@ -1,6 +1,32 @@
 /* global PIXI */
 
 export default class MapView {
+  drawHexGrid(step) {
+    const c = step / 2;
+    const a = c / 2;
+    const b = Math.sin(60 * Math.PI / 180.0) * c;
+    let even = true;
+    for (let y = 0; y < this.h - step; y += b) {
+      for (let x = even ? 0 : a + c; x < this.w - step; x += step + c) {
+        /*
+        const arr = [x, y + b,
+          x + a, y,
+          x + a + c, y,
+          x + 2 * c, y + b];
+//          x + a + c, y + 2 * b,
+//          x + a, y + 2 * b];
+//        const al = arr.length;
+*/
+        this.graphics.moveTo(x, y + b);
+        this.graphics.lineTo(x + a, y);
+        this.graphics.lineTo(x + a + c, y);
+        this.graphics.lineTo(x + 2 * c, y + b);
+//        this.graphics.moveTo(arr[al - 2], arr[al - 1]);
+//        this.graphics.drawPolygon(arr);
+      }
+      even = !even;
+    }
+  }
   constructor(map, parent) {
     this.map = map;
     this.w = 800;
@@ -10,6 +36,8 @@ export default class MapView {
     const stage = new PIXI.Container();
     stage.interactive = true;
     const graphics = new PIXI.Graphics();
+    this.graphics = graphics;
+    graphics.alpha = 0.25;
     graphics.lineStyle(2, 0xFFFFFF, 1);
     const step = 64;
     for (let y = step; y < this.w; y += step) {
@@ -20,7 +48,7 @@ export default class MapView {
       graphics.moveTo(x, 0);
       graphics.lineTo(x, this.h);
     }
-    graphics.alpha = 0.25;
+    this.drawHexGrid(64);
     stage.addChild(graphics);
     renderer.render(stage);
   }
