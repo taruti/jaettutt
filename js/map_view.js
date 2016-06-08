ab2b13d9f929be0ab4e88caff817eafbfe152cc3
Taru Karttunen <taruti@taruti.net>
2016-06-09 00:07:09 +0300

    Update map_views for grid.js

--- js/map_view.js
+++ js/map_view.js
@@ -1,28 +1,8 @@
 /* global PIXI */
 
+import Grid from './grid.js';
+
 export default class MapView {
-  drawHexGrid(step) {
-    const c = step / 2;
-    const a = c / 2;
-    const b = Math.sin(60 * Math.PI / 180.0) * c;
-    function clasp(x, lim) {
-      return x > lim ? lim : x;
-    }
-    let odd = false;
-    for (let y = 0; y < this.h; y += b) {
-      if (odd) {
-        this.graphics.moveTo(clasp(0, this.w), clasp(y, this.h));
-        this.graphics.lineTo(clasp(a, this.w), clasp(y + b, this.h));
-      }
-      for (let x = odd ? a + c : 0; x < this.w; x += step + c) {
-        this.graphics.moveTo(clasp(x, this.w), clasp(y + b, this.h));
-        this.graphics.lineTo(clasp(x + a, this.w), clasp(y, this.h));
-        this.graphics.lineTo(clasp(x + a + c, this.w), clasp(y, this.h));
-        this.graphics.lineTo(clasp(x + 2 * c, this.w), clasp(y + b, this.h));
-      }
-      odd = !odd;
-    }
-  }
   constructor(map, parent) {
     this.map = map;
     this.w = 800;
@@ -31,21 +11,10 @@ export default class MapView {
     parent.appendChild(renderer.view);
     const stage = new PIXI.Container();
     stage.interactive = true;
-    const graphics = new PIXI.Graphics();
-    this.graphics = graphics;
-    graphics.alpha = 0.25;
-    graphics.lineStyle(2, 0xFFFFFF, 1);
-    const step = 64;
-    for (let y = step; y < this.w; y += step) {
-      graphics.moveTo(0, y);
-      graphics.lineTo(this.w, y);
-    }
-    for (let x = step; x < this.w; x += step) {
-      graphics.moveTo(x, 0);
-      graphics.lineTo(x, this.h);
-    }
-    this.drawHexGrid(64);
-    stage.addChild(graphics);
+    this.grid = new Grid(this.w, this.h, 64);
+    this.grid.drawSquare();
+    this.grid.drawHex();
+    stage.addChild(this.grid.graphics);
     renderer.render(stage);
   }
 }
